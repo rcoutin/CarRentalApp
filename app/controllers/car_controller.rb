520a898d207d6623eb1eda714a1209a45f2bfacd
Vijay Hebbar <vhhebbar@ncsu.edu>
2017-09-23 00:21:06 -0400

    Adding car routes and views
    Adding car routes and views
    
    But there's still an error No route matches {:action=>"show", :controller=>"car"}, missing required keys: [:id]

--- app/controllers/car_controller.rb
+++ app/controllers/car_controller.rb
@@ -1,29 +1,30 @@
 class CarController < ApplicationController
 
-  def show
-
-  end
   def index
+    @car = Car.all
+  end
 
+  def show
+    puts "the params ares"
+    puts params
+    @car = Car.find(params[:id])
   end
 
   def new
-
   end
 
   def create
-  @car = Car.new(car_params)
+  @car = Car.new(params.require(:car).permit(:manufacturer, :model_name, :license_number))
 
     if @car.save
       redirect @car
     else
       render plain: params[:car]
     end
-
   end
 
   private
   def car_params
-    params.require(:@car).permit(:manufacturer, :model_name, :license_number)
+    params.require(:car).permit(:manufacturer, :model_name, :license_number)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170921212313) do
+ActiveRecord::Schema.define(version: 20170923012341) do
+
+  create_table "cars", force: :cascade do |t|
+    t.string "manufacturer"
+    t.string "model_name"
+    t.string "license_number"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
