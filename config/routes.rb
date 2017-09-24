a914c9504b1a3feebce5c08381de6928b932f0ab
Rajan Alwan <rajan.alwan@gmail.com>
2017-09-24 10:00:59 -0400

    Adding route to car/index from customers/show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'unauthorized/show'
   get 'application/sign_out'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :customers
   resources :login, only: [:new, :create], as: :login
 
