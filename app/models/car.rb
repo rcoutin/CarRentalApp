aee0b3c33e64a24f70592b3ad096994791efa9cc
Vijay Hebbar <vhhebbar@ncsu.edu>
2017-09-24 17:32:04 -0400

    Validations for Car

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,7 +2,7 @@ class Car < ApplicationRecord
 
   validates :license_number, presence: true, length: {is: 7}
   validates :manufacturer, presence: true
-  validates :model, presence: true, inclusion {in: ""}
+  validates :model, presence: true, inclusion: {in: {"Sedan", "Coupe", "SUV"}
   validates :style, presence: true
   has_one :reservation
 end
